484e9cbb25610a53ef0410f7ed4653c017d17116
zmdong <52513772+zmdong@users.noreply.github.com>
2019-07-15 23:31:46 +0800

    Create printf binary.cpp

---
+++ printf binary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <numeric>
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+using namespace std;
+int main(){
+	int x = 0b10101100;
+	printf(BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(x));
+    return x; 
+}
